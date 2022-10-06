056d2248e32aa41bd0b3acd654149fb8a97591c1
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-10-06 12:23:34 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "rmstudio_dev_37287",
   url: "https://rmstudio_dev_37287.botics.co",
   api: "https://rmstudio_dev_37287.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
